997eb57f81c04586909870c1faa01f42b7636f3c
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-01-29 23:29:24 +0100

    [UPD] done seeders with faker

--- database/seeders/AziendaSeeder.php
+++ database/seeders/AziendaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class AziendaSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class AziendaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('aziendas')->insert([
+                'nome' => $faker->company,
+                'logo' => $faker->imageUrl(640, 480, 'business'),
+                'partita_iva' => $faker->numerify('###########'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
     }
 }
--- database/seeders/DipendenteSeeder.php
+++ database/seeders/DipendenteSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DipendenteSeeder extends Seeder
 {
@@ -14,6 +16,20 @@ class DipendenteSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        $aziende = DB::table('aziendas')->pluck('id');
+
+        foreach (range(1, 30) as $index) {
+            DB::table('dipendentes')->insert([
+                'nome' => $faker->firstName,
+                'cognome' => $faker->lastName,
+                'aziendas_id' => $faker->randomElement($aziende),
+                'telefono' => $faker->phoneNumber,
+                'email' => $faker->unique()->safeEmail,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
